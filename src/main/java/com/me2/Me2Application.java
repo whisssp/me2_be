18f84b366a86664ee53f76d34bcfc286632095ed
Whisper <nghia14802@gmail.com>
2024-03-15 22:27:36 +0700

    initial commit

---
+++ src/main/java/com/me2/Me2Application.java
@@ -0,0 +1,13 @@
+package com.me2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Me2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Me2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/me2/Me2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Me2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
