be27a1461e68278cabe201851083c8b832551093
Whisper <nghia14802@gmail.com>
2024-03-18 09:48:22 +0700

    add config liquibase, create table users

--- src/main/java/com/me2/Me2Application.java
+++ src/main/java/com/me2/Me2Application.java
@@ -1,13 +1,29 @@
 package com.me2;
 
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LiquibaseException;
+import liquibase.integration.spring.SpringLiquibase;
+import liquibase.resource.ClassLoaderResourceAccessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Me2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Me2Application.class, args);
     }
-
 }
