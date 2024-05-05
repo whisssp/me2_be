a69b71ae9bc12cd47909ce8a3c79890ca1d95782
Whisper <nghia14802@gmail.com>
2024-05-05 15:29:41 +0700

    modified audit type, add api create category for admin

---
+++ src/main/java/com/me2/service/CategoryService.java
@@ -0,0 +1,20 @@
+package com.me2.service;
+
+import com.me2.rest.vm.CategoryVM;
+import com.me2.service.dto.CategoryDTO;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    void create(List<CategoryDTO> dtoList);
+
+    CategoryVM update(List<CategoryDTO> dtoList);
+
+    void delete(List<Long> ids);
+
+    CategoryVM getOneById(Long id);
+
+    List<CategoryVM> getAll(Pageable pageable);
+}
